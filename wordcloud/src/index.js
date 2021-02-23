1446203cbe8d949bf1e20c1a2904a7826c6b4dee
Joshua Cleveland <joelcld05@gmail.com>
2021-02-22 23:48:05 -0500

    visualizaciones

---
+++ wordcloud/src/index.js
@@ -0,0 +1,68 @@
+const dscc = require('@google/dscc');
+const viz = require('@google/dscc-scripts/viz/initialViz.js');
+const local = require('/src/localMessage.js');
+const am4core = require('@amcharts/amcharts4/core.js');
+const wordCloud = require('@amcharts/amcharts4/plugins/wordCloud');
+
+//require('highcharts/modules/exporting')(Highcharts);
+
+var containerElement = document.createElement('div');
+containerElement.id = 'container';
+const height = dscc.getHeight();
+const width = dscc.getWidth();
+containerElement.style.height = height + "px";
+containerElement.style.width = width + "px";
+document.body.appendChild(containerElement);
+
+export const LOCAL = false;
+// write viz code here
+const drawViz = (data) => {
+  
+  var textString= "";
+
+  data.tables.DEFAULT.forEach(element => {
+    var stringL=parseInt(element.metricID[0]);
+    stringL=stringL>=50?50:stringL;
+    textString = textString + (element.dimID[0] + " ").repeat(parseInt(element.metricID[0]));
+  });
+  
+
+  am4core.ready(function() {
+
+    var chart = am4core.create("container", wordCloud.WordCloud);
+    var series = chart.series.push(new wordCloud.WordCloudSeries());
+    chart.height=height;
+    chart.width=width;
+    series.accuracy = 4;
+    series.step = 15;
+    series.rotationThreshold = 0.7;
+    series.maxCount = 200;
+    series.minWordLength = 2;
+    series.labels.template.margin(4,4,4,4);
+    series.maxFontSize = am4core.percent(30);
+    
+    series.text = textString;
+
+    series.colors = new am4core.ColorSet();
+    series.colors.passOptions = {}; // makes it loop
+    
+    //series.labelsContainer.rotation = 45;
+    series.angles = [0,0];
+    series.fontWeight = "700"
+    
+    setInterval(function () {
+      series.dataItems.getIndex(Math.round(Math.random() * (series.dataItems.length - 1))).setValue("value", Math.round(Math.random() * 10));
+     }, 1000*1000)
+    
+    });
+    
+ 
+
+};
+
+// renders locally
+if (LOCAL) {
+  drawViz(local.message);
+} else {
+  dscc.subscribeToData(drawViz, { transform: dscc.objectTransform });
+}
---
+++ wordcloud/src/localMessage.js
@@ -0,0 +1,283 @@
+export const message = {
+  "tables": {
+    "DEFAULT": [
+      {
+        "dimID": [
+          "Opinión sobre SM"
+        ],
+        "metricID": [
+          "5"
+        ]
+      },
+      {
+        "dimID": [
+          "Vinculación con política"
+        ],
+        "metricID": [
+          "556"
+        ]
+      },
+      {
+        "dimID": [
+          "Stanley Motta"
+        ],
+        "metricID": [
+          "78"
+        ]
+      },
+      {
+        "dimID": [
+          "Negocios"
+        ],
+        "metricID": [
+          "8725"
+        ]
+      },
+      {
+        "dimID": [
+          "Vinculación con el gobierno"
+        ],
+        "metricID": [
+          "59"
+        ]
+      },
+      {
+        "dimID": [
+          "Fundación Alberto Motta"
+        ],
+        "metricID": [
+          "1"
+        ]
+      },
+      {
+        "dimID": [
+          "Museo de la Mola"
+        ],
+        "metricID": [
+          "2"
+        ]
+      },
+      {
+        "dimID": [
+          "Gobierno Panameñista"
+        ],
+        "metricID": [
+          "3"
+        ]
+      },
+      {
+        "dimID": [
+          "Caso Ricardo Martinelli"
+        ],
+        "metricID": [
+          "4"
+        ]
+      },
+      {
+        "dimID": [
+          "Casos de corrupción"
+        ],
+        "metricID": [
+          "5"
+        ]
+      }
+    ]
+  },
+  "fields": {
+    "dimID": [
+      {
+        "id": "qt_6mtnnmw9gc",
+        "name": "tag",
+        "type": "TEXT",
+        "concept": "DIMENSION"
+      }
+    ],
+    "metricID": [
+      {
+        "id": "qt_zeunnmw9gc",
+        "name": "Id",
+        "type": "NUMBER",
+        "concept": "METRIC"
+      }
+    ]
+  },
+  "style": {},
+  "theme": {
+    "themeFillColor": {
+      "color": "#eceff1",
+      "themeRef": {
+        "index": 0
+      }
+    },
+    "themeFontColor": {
+      "color": "#000000",
+      "themeRef": {
+        "index": 1
+      }
+    },
+    "themeFontFamily": "Montserrat",
+    "themeAccentFillColor": {
+      "color": "#263238",
+      "themeRef": {
+        "index": 2
+      }
+    },
+    "themeAccentFontColor": {
+      "color": "#EFEFEF",
+      "themeRef": {
+        "index": 3
+      }
+    },
+    "themeAccentFontFamily": "Roboto",
+    "themeSeriesColor": [
+      {
+        "color": "#0072f0",
+        "seriesRef": {
+          "index": 0
+        },
+        "themeRef": {
+          "index": 4
+        }
+      },
+      {
+        "color": "#00b6cb",
+        "seriesRef": {
+          "index": 1
+        },
+        "themeRef": {
+          "index": 5
+        }
+      },
+      {
+        "color": "#f10096",
+        "seriesRef": {
+          "index": 2
+        },
+        "themeRef": {
+          "index": 6
+        }
+      },
+      {
+        "color": "#f66d00",
+        "seriesRef": {
+          "index": 3
+        },
+        "themeRef": {
+          "index": 7
+        }
+      },
+      {
+        "color": "#ffa800",
+        "seriesRef": {
+          "index": 4
+        },
+        "themeRef": {
+          "index": 8
+        }
+      },
+      {
+        "color": "#7cb342",
+        "seriesRef": {
+          "index": 5
+        },
+        "themeRef": {
+          "index": 9
+        }
+      },
+      {
+        "color": "#5e35b1",
+        "seriesRef": {
+          "index": 6
+        }
+      },
+      {
+        "color": "#03a9f4",
+        "seriesRef": {
+          "index": 7
+        }
+      },
+      {
+        "color": "#ec407a",
+        "seriesRef": {
+          "index": 8
+        }
+      },
+      {
+        "color": "#ff7043",
+        "seriesRef": {
+          "index": 9
+        }
+      },
+      {
+        "color": "#737373",
+        "seriesRef": {
+          "index": 10
+        }
+      },
+      {
+        "color": "#F15A60",
+        "seriesRef": {
+          "index": 11
+        }
+      },
+      {
+        "color": "#7AC36A",
+        "seriesRef": {
+          "index": 12
+        }
+      },
+      {
+        "color": "#5A9BD4",
+        "seriesRef": {
+          "index": 13
+        }
+      },
+      {
+        "color": "#FAA75A",
+        "seriesRef": {
+          "index": 14
+        }
+      },
+      {
+        "color": "#9E67AB",
+        "seriesRef": {
+          "index": 15
+        }
+      },
+      {
+        "color": "#CE7058",
+        "seriesRef": {
+          "index": 16
+        }
+      },
+      {
+        "color": "#D77FB3",
+        "seriesRef": {
+          "index": 17
+        }
+      },
+      {
+        "color": "#81D4FA",
+        "seriesRef": {
+          "index": 18
+        }
+      },
+      {
+        "color": "#F48FB1",
+        "seriesRef": {
+          "index": 19
+        }
+      }
+    ],
+    "themeIncreaseColor": {
+      "color": "#2E7D32"
+    },
+    "themeDecreaseColor": {
+      "color": "#D32F2F"
+    },
+    "themeGridColor": {
+      "color": "#e0e0e0"
+    }
+  },
+  "interactions": {}
+};
