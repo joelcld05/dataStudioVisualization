0e3144d5971c9617fcd71fdb2558b14ac5ae0194
Joshua Cleveland <joelcld05@gmail.com>
2021-06-17 00:32:30 -0500

    mejoras

--- networkcluster/src/index.js
+++ networkcluster/src/index.js
@@ -1,5 +1,5 @@
 const dscc = require('@google/dscc');
-const viz = require('@google/dscc-scripts/viz/initialViz.js');
+//const viz = require('@google/dscc-scripts/viz/initialViz.js');
 const local = require('./localMessage.js');
 const visData = require('vis-data');
 const visNetwork = require('vis-network');
@@ -12,23 +12,28 @@ const visNetwork = require('vis-network');
 export const LOCAL = false;
 
 const drawViz = (data) => {
+  console.log("🚀 ~ file: index.js ~ line 15 ~ drawViz ~ data", data)
 
   var fields = data.fields;
-  var stry=data.tables.DEFAULT[0].metricID[0]
-  .replace("},];", "}]")
-  .replace(/:'/g,':"')
-  .replace(/',/g,'",')
-  .replace(/{/g,'{"')
-  .replace(/, /g,',"')
-  .replace(/:/g,'":');
+  var stry = data.tables.DEFAULT[0].dimID[0]
+    .replace("},];", "}]")
+    .replace(/:'/g, ':"')
+    .replace(/',/g, '",')
+    .replace(/{/g, '{"')
+    .replace(/, /g, ',"')
+    .replace(/:/g, '":');
 
-  var strx=data.tables.DEFAULT[1].metricID[0]
-  .replace("},];", "}]")
-  .replace(/{/g,'{"')
-  .replace(/, /g,',"')
-  .replace(/:/g,'":')
-  .replace(/:'/g,':"')
-  .replace(/',/g,'",')
+  var strx = data.tables.DEFAULT[1].dimID[0]
+    .replace("},];", "}]")
+    .replace(/{/g, '{"')
+    .replace(/, /g, ',"')
+    .replace(/:/g, '":')
+    .replace(/:'/g, ':"')
+    .replace(/',/g, '",')
+
+  var mentions = data.tables.DEFAULT[1].metricID[0]
+    .replace("},];", "}]");
+
 
   let yData = JSON.parse(stry);
   let link = JSON.parse(strx);
@@ -40,49 +45,50 @@ const drawViz = (data) => {
   var close = document.createElement('div');
   var content = document.createElement('div');
   var loader = document.createElement('div');
-  
-  function createDialod(){
-    dialog.id="dialog";
-    dialogHeader.id="dialogHeader";
-    content.id="dialogContent";
 
-    title.id= "titleHeader";
-    title.textContent="Header Title";
-    close.addEventListener("click",closeDialog);
-    close.textContent="X"
-    close.id="closebtn";
+  function createDialod() {
+    dialog.id = "dialog";
+    dialogHeader.id = "dialogHeader";
+    content.id = "dialogContent";
+
+    title.id = "titleHeader";
+    title.textContent = "Header Title";
+    close.addEventListener("click", closeDialog);
+    close.textContent = "X"
+    close.id = "closebtn";
     dialogHeader.append(title);
     dialogHeader.append(close);
 
-    loader.className="loader";
+    loader.className = "loader";
     //content.append(loader);
-    createMention();
-    createMention();
+    //createMention();
+    //createMention();
+    content.textContent = mentions;
 
     dialog.append(dialogHeader);
     dialog.append(content);
     document.body.prepend(dialog);
   }
 
-  function createMention(){
+  function createMention() {
     var mention = document.createElement('div');
-    mention.className ="mentionBox";
+    mention.className = "mentionBox";
     var mentiondatacontent = document.createElement('div');
-    mentiondatacontent.className="mentionContentBox";
+    mentiondatacontent.className = "mentionContentBox";
     var image = document.createElement('div');
-    image.className="imageMentionBox";
+    image.className = "imageMentionBox";
     var imagetag = document.createElement('img');
-    imagetag.className="imagenUsuario";
-    imagetag.src ="https://www.vippng.com/png/detail/202-2026524_person-icon-default-user-icon-png.png";
+    imagetag.className = "imagenUsuario";
+    imagetag.src = "https://www.vippng.com/png/detail/202-2026524_person-icon-default-user-icon-png.png";
     image.append(imagetag);
     var mentiondata = document.createElement('div');
-    mentiondata.className="mentionData";
+    mentiondata.className = "mentionData";
     var mentiontext = document.createElement('div');
-    mentiontext.className="metiontext";
-    var userMention=document.createElement('p');
-    userMention.textContent="@usuario";
-    var userTxt=document.createElement('p');
-    userTxt.textContent="Mira Luiscito, aqui no tiene nada que ver quien es el patron, ni tiene que ver quien lo denuncia, el hecho es que es corrupción aunque sea el niño dios el que lo haga y hay que denunciarlo, aunque no vaya a pasar nada…";
+    mentiontext.className = "metiontext";
+    var userMention = document.createElement('p');
+    userMention.textContent = "@usuario";
+    var userTxt = document.createElement('p');
+    userTxt.textContent = "Mira Luiscito, aqui no tiene nada que ver quien es el patron, ni tiene que ver quien lo denuncia, el hecho es que es corrupción aunque sea el niño dios el que lo haga y hay que denunciarlo, aunque no vaya a pasar nada…";
     mentiontext.append(userMention);
     mentiontext.append(userTxt);
     mentiondatacontent.append(mentiondata);
@@ -93,29 +99,51 @@ const drawViz = (data) => {
   }
 
   function interactionFilter(id) {
-
     const interactionId = "interactionsConfigId";
     const dimensionId = fields.dimID[0].id;
     const FILTER = dscc.InteractionType.FILTER;
-    const xval=yData.find(x=>{
-      return x.id===id
-    });
-    
     let interactionData = {
       "concepts": [dimensionId],
-      "values": [[xval.id+""]]
+      "values": [[id + ""]]
     };
+    //dscc.sendInteraction(interactionId, FILTER, interactionData);
 
-    dscc.sendInteraction(interactionId, FILTER, interactionData);
+    var componentId = dscc.getComponentId();
+    var interactionMessage = {
+      type: dscc.ToDSMessageType.INTERACTION,
+      id: interactionId,
+      data: interactionData,
+      componentId: componentId,
+    };
+    console.log(interactionMessage);
+    window.parent.postMessage(interactionMessage, '*');
+
 
   }
 
-  function showDialog(id){
+  /*function getDarkColor() {
+    var allowed = "ABCDEF0123456789", S = "#";
+    while(S.length < 7){
+        S += allowed.charAt(Math.floor((Math.random()*100)+1));
+    }
+    return S;
+  }*/
+
+  var colors = ["#B927CB", "#524CE5", "#468434", "#8523C3", "#81CED6", "#4F8605", "#DBB401", "#E25744", "#22412D", "#4BD680", "#E2295B", "#464382", "#4B3410", "#4B3DBE", "#55DCF3", "#1C2BD9", "#96CD66", "#F353B0", "#0997C4", "#F203E8", "#434194", "#68B1F5", "#C83114", "#0BF5B6", "#680EEC", "#3CDE65", "#F11F18", "#356710", "#416DBB", "#F966F9", "#C9E792", "#6B7E21", "#37E70E", "#C5EE4C", "#DD182E", "#91F395", "#396977", "#888068", "#4F208F", "#77F54F", "#B203EE", "#F23930", "#E71183", "#D66987", "#647D75", "#98B371", "#B28756", "#57786F", "#D6645E", "#9229B0", "#536935", "#6024B4", "#53CBFB", "#BB8340", "#619059", "#5F5849", "#E1B875", "#8814BF", "#337F5F", "#177F22", "#30E323", "#929713", "#3F9181", "#2E6E94", "#0768C9", "#5876D4", "#CE8C06", "#852B7C", "#CFB559", "#6ED3C4", "#2CBBDE", "#9491C4", "#9D608B", "#42182B", "#562B73", "#710325", "#DE0FB1", "#EFFC11", "#C08567", "#27830D", "#B58CCB", "#CC353D", "#963D54", "#F139D1", "#EBF456", "#D38437", "#B3F30B", "#58E436", "#87EBFC", "#15F238", "#711E46", "#B00B1F", "#44545E", "#4027E1", "#8E2741", "#E318D6", "#93B028", "#80FE80", "#92E9C2", "#CD9158", "#C79DCB", "#BC4588", "#C2083F", "#F2FCC6", "#E98605", "#2004B6", "#E75DB5", "#D1886B", "#5CFE64", "#1F68C1", "#5FD2FB", "#5C34DC", "#32D7F2", "#24B277", "#98F989", "#47E26B", "#83CE35", "#6BCCBD", "#40C0C7", "#4D5F06", "#C6E209", "#75CF25", "#38C06F", "#C5E61C", "#31C71C", "#083709", "#B8F6E3", "#11B6F7", "#E781B5", "#296FE6", "#CC8548", "#C2942C", "#998F39", "#21C719", "#22D0E5", "#95414F", "#8D1F0F", "#7C758E", "#597C5B", "#60D1CE", "#BC8330", "#E5F0BC", "#DF0B6E", "#D50851", "#79C800", "#0DD754", "#C148EF", "#D92E3B", "#CF6533", "#D9661E", "#B39147", "#D920EF", "#D9658D", "#885853", "#245B54", "#D4360D", "#9F8910", "#CE5B28", "#4DD630", "#0723D2", "#0B6B7F", "#37E96C", "#13C6F8", "#709D07", "#5D14F9", "#E88968", "#219646", "#FDDF8E", "#C033FE", "#ED2523", "#0F68B7", "#E63747", "#5923B9", "#D39C97", "#BC4459", "#DD04BB", "#0B9D4E", "#E30780", "#5BDE26", "#E7D01F", "#BC7600", "#ECDB6B", "#18EE80", "#11F31C", "#8FC06C", "#04B4DE", "#BD0420", "#218DC5", "#0FD693", "#354FFB", "#CE7E36", "#0D95CE", "#99D21E", "#171EF3", "#B6FDBE", "#E695C0", "#0D1F21", "#261D92", "#965930", "#489C59", "#36E742"];
+  /*for(var i=0;i<=200;i++){
+    colors.push(getDarkColor());
+  }*/
+
+  //console.log("🚀 ~ file: index.js ~ line 121 ~ colors", JSON.stringify(colors))
+
+
+
+  function showDialog(id) {
     var dialog = document.getElementById('dialog');
     dialog.style.display = "block";
   }
 
-  function closeDialog(){
+  function closeDialog() {
     var dialog = document.getElementById('dialog');
     dialog.style.display = "none";
   }
@@ -123,91 +151,17 @@ const drawViz = (data) => {
   let error;
   let colorGroups = {};
 
-  try {
-    document.getElementById('container').remove();
-
-
-    document.getElementById('dialog').remove();
-  } catch (e) {
-    error = "";
-  }
-  //createDialod();
-
-  var containerElement = document.createElement('div');
-  containerElement.id = 'container';
-  //const margin = { top: 10, bottom: 50, right: 10, left: 10 };
-  const height = dscc.getHeight();
-  const width = dscc.getWidth();
-  containerElement.style.height = height + "px";
-  containerElement.style.width = width + "px";
-  document.body.appendChild(containerElement);
 
   let merge = {};
 
-  var colors = [];
-  
- /* while (colors.length < 100) {
-    do {
-      var color = Math.floor((Math.random() * 50000000) + 1);
-    } while (colors.indexOf(color) >= 0);
-    colors.push("#" + ("000000" + color.toString(16)).slice(-6));
-  }*/
-
-  var colors=[
-    "#421f79", "#7a3d93", "#635f6d", "#93f2d7", "#9b5c2a", "#15b9ee", "#0f5997",
-    "#409188", "#911e20", "#1350ce", "#10e5b1", "#fff4d7", "#cb2582", "#ce00be",
-    "#32d5d6", "#17232", "#608572", "#c79bc2", "#00f87c", "#77772a", "#6995ba",
-    "#fc6b57", "#f07815", "#8fd883", "#060e27", "#96e591", "#21d52e", "#d00043",
-    "#b47162", "#1ec227", "#4f0f6f", "#1d1d58", "#947002", "#bde052", "#e08c56",
-    "#28fcfd", "#bb09b", "#36486a", "#d02e29", "#1ae6db", "#3e464c", "#a84a8f",
-    "#63b598", "#ce7d78", "#ea9e70", "#a48a9e", "#c6e1e8", "#648177" ,"#0d5ac1" ,
-    "#f205e6" ,"#1c0365" ,"#14a9ad" ,"#4ca2f9" ,"#a4e43f" ,"#d298e2" ,"#6119d0",
-    "#d2737d" ,"#c0a43c" ,"#f2510e" ,"#651be6" ,"#79806e" ,"#61da5e" ,"#cd2f00" ,
-    "#9348af" ,"#01ac53" ,"#c5a4fb" ,"#996635","#b11573" ,"#4bb473" ,"#75d89e" ,
-    "#2f3f94" ,"#2f7b99" ,"#da967d" ,"#34891f" ,"#b0d87b" ,"#ca4751" ,"#7e50a8" ,
-    "#c4d647" ,"#e0eeb8" ,"#11dec1" ,"#289812" ,"#566ca0" ,"#ffdbe1" ,"#2f1179" ,
-    "#935b6d" ,"#916988" ,"#513d98" ,"#aead3a", "#9e6d71", "#4b5bdc", "#0cd36d",
-    "#250662", "#cb5bea", "#228916", "#ac3e1b", "#df514a", "#539397", "#880977",
-    "#f697c1", "#ba96ce", "#679c9d", "#c6c42c", "#5d2c52", "#48b41b", "#e1cf3b",
-    "#5be4f0", "#57c4d8", "#a4d17a", "#225b8", "#be608b", "#96b00c", "#088baf",
-    "#f158bf", "#e145ba", "#ee91e3", "#05d371", "#5426e0", "#4834d0", "#802234",
-    "#6749e8", "#0971f0", "#8fb413", "#b2b4f0", "#c3c89d", "#c9a941", "#41d158",
-    "#fb21a3", "#51aed9", "#5bb32d", "#807fb", "#21538e", "#89d534", "#d36647",
-    "#7fb411", "#0023b8", "#3b8c2a", "#986b53", "#f50422", "#983f7a", "#ea24a3",
-    "#79352c", "#521250", "#c79ed2", "#d6dd92", "#e33e52", "#b2be57", "#fa06ec",
-    "#1bb699", "#6b2e5f", "#64820f", "#1c271", "#21538e", "#89d534", "#d36647",
-    "#7fb411", "#0023b8", "#3b8c2a", "#986b53", "#f50422", "#983f7a", "#ea24a3",
-    "#79352c", "#521250", "#c79ed2", "#d6dd92", "#e33e52", "#b2be57", "#fa06ec",
-    "#1bb699", "#6b2e5f", "#64820f", "#1c271", "#9cb64a", "#996c48", "#9ab9b7",
-    "#06e052", "#e3a481", "#0eb621", "#fc458e", "#b2db15", "#aa226d", "#792ed8",
-    "#73872a", "#520d3a", "#cefcb8", "#a5b3d9", "#7d1d85", "#c4fd57", "#f1ae16",
-    "#8fe22a", "#ef6e3c", "#243eeb", "#1dc18", "#dd93fd", "#3f8473", "#e7dbce",
-    "#421f79", "#7a3d93", "#635f6d", "#93f2d7", "#9b5c2a", "#15b9ee", "#0f5997",
-    "#409188", "#911e20", "#1350ce", "#10e5b1", "#fff4d7", "#cb2582", "#ce00be",
-    "#32d5d6", "#17232", "#608572", "#c79bc2", "#00f87c", "#77772a", "#6995ba",
-    "#fc6b57", "#f07815", "#8fd883", "#060e27", "#96e591", "#21d52e", "#d00043",
-    "#b47162", "#1ec227", "#4f0f6f", "#1d1d58", "#947002", "#bde052", "#e08c56",
-    "#28fcfd", "#bb09b", "#36486a", "#d02e29", "#1ae6db", "#3e464c", "#a84a8f",
-    "#911e7e", "#3f16d9", "#0f525f", "#ac7c0a", "#b4c086", "#c9d730", "#30cc49",
-    "#3d6751", "#fb4c03", "#640fc1", "#62c03e", "#d3493a", "#88aa0b", "#406df9",
-    "#615af0", "#4be47", "#2a3434", "#4a543f", "#79bca0", "#a8b8d4", "#00efd4",
-    "#7ad236", "#7260d8", "#1deaa7", "#06f43a", "#823c59", "#e3d94c", "#dc1c06",
-    "#f53b2a", "#b46238", "#2dfff6", "#a82b89", "#1a8011", "#436a9f", "#1a806a",
-    "#4cf09d", "#c188a2", "#67eb4b", "#b308d3", "#fc7e41", "#af3101", "#ff065",
-    "#71b1f4", "#a2f8a5", "#e23dd0", "#d3486d", "#00f7f9", "#474893", "#3cec35",
-    "#1c65cb", "#5d1d0c", "#2d7d2a", "#ff3420", "#5cdd87", "#a259a4", "#e4ac44",
-    "#1bede6", "#8798a4", "#d7790f", "#b2c24f", "#de73c2", "#d70a9c", "#25b67",
-    "#88e9b8", "#c2b0e2", "#86e98f", "#ae90e2", "#1a806b", "#436a9e", "#0ec0ff",
-    "#f812b3", "#b17fc9", "#8d6c2f", "#d3277a", "#2ca1ae", "#9685eb", "#8a96c6",
-    "#dba2e6", "#76fc1b", "#608fa4", "#20f6ba", "#07d7f6", "#dce77a", "#77ecca"]
-
   yData.forEach((item) => {
-    if(!item.font){
-      item.font={}
+    if (!item.font) {
+      item.font = {}
     }
     item.font.strokeColor = "white";
     item.font.strokeWidth = 5;
-    item.opacity=0.1
+    item.opacity = 0.1
+
     if (item.group in colorGroups) {
       item.font.color = colorGroups[item.group];
       item.color = colorGroups[item.group];
@@ -218,6 +172,8 @@ const drawViz = (data) => {
       item.color = colors[count];
     }
   })
+
+
 
   try {
     let semiColonArray = data.style.excludeWords.value.split(';');
@@ -258,12 +214,7 @@ const drawViz = (data) => {
   var nodes = new visData.DataSet(yData);
   var edges = new visData.DataSet(link);
 
-  var container = document.getElementById("container");
-
-  var data = {
-    nodes: nodes,
-    edges: edges,
-  };
+  var data = { nodes: nodes, edges: edges, };
   var options = {
     physics: {
       forceAtlas2Based: {
@@ -278,13 +229,41 @@ const drawViz = (data) => {
       stabilization: { iterations: 150 },
     },
   };
-  let networkG = new visNetwork.Network(container, data, options);
-  networkG.on('click',(params)=>{    
-    let node = networkG.getNodeAt(params.pointer.DOM);
-    //showDialog(node);
-    interactionFilter(node);
-  });
+
+  try {
+
+    if (!document.getElementById('container')) {
+      var containerElement = document.createElement('div');
+      containerElement.id = 'container';
+      //const margin = { top: 10, bottom: 50, right: 10, left: 10 };
+      const height = dscc.getHeight();
+      const width = dscc.getWidth();
+      containerElement.style.height = height + "px";
+      containerElement.style.width = width + "px";
+      document.body.appendChild(containerElement);
+
+      var container = document.getElementById("container");
+      let networkG = new visNetwork.Network(container, data, options);
+      networkG.on('click', (params) => {
+
+        let node = networkG.getNodeAt(params.pointer.DOM);
+        if (node) {
+          interactionFilter(node);
+        }
+      });
+
+    }
+
+
+  } catch (e) {
+    error = "";
+  }
+
+
+
+
 };
+
 
 // renders locally
 if (LOCAL) {
@@ -292,3 +271,14 @@ if (LOCAL) {
 } else {
   dscc.subscribeToData(drawViz, { transform: dscc.objectTransform });
 }
+
+
+window.addEventListener("message", (event) => {
+console.log("🚀 ~ file: index.js ~ line 277 ~ window.addEventListener ~ event", event)
+  
+
+  // ...
+}, false);
+
+
+https://datastudio.google.com/u/0/batchedDataV2?appVersion=20210616_00020056
